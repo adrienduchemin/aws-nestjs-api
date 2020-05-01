fdf0c12b0fd03c26ea0b52ab9f331209c5f48332
Adrien Duchemin <duchemin.adrien@gmail.com>
2020-05-01 23:38:07 +0200

    initial commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'prettier',
     'prettier/@typescript-eslint',
+    'plugin:import/errors',
+    'plugin:import/typescript',
   ],
   root: true,
   env: {
@@ -20,5 +22,25 @@ module.exports = {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      { ignoreRestSiblings: true },
+    ],
+    semi: 'off',
+    '@typescript-eslint/semi': ['warn', 'never'],
+    'import/no-absolute-path': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'import/order': [
+      'error',
+      {
+        'newlines-between': 'ignore',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
   },
 };
---
+++ src/lambda.ts
@@ -0,0 +1,33 @@
+import { Server } from 'http'
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
+import { Handler, Context, APIGatewayProxyEvent } from 'aws-lambda'
+import { createServer, proxy } from 'aws-serverless-express'
+
+import { AppModule } from './app.module'
+
+const instanceId: string = Math.random()
+  .toString(16)
+  .slice(2)
+console.log('Starting lambda', { instanceId })
+
+// NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
+// due to a compressed response (e.g. gzip) which has not been handled correctly
+// by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
+// binaryMimeTypes below
+const binaryMimeTypes: string[] = []
+let cachedServer: Server
+
+async function bootstrapServer(): Promise<Server> {
+    const nestApp = await NestFactory.create<NestExpressApplication>(AppModule)
+    await nestApp.init()
+    cachedServer = createServer(nestApp.getHttpAdapter().getInstance(), undefined, binaryMimeTypes)
+    return cachedServer
+}
+
+export const handler: Handler = async (event: APIGatewayProxyEvent, context: Context) => {
+    console.log('Handler called', { event, context, instanceId })
+    const server = cachedServer ? cachedServer : await bootstrapServer()
+    // await proxy(server, event, context, 'PROMISE').promise
+     return proxy(server, event, context, 'PROMISE').promise
+}
