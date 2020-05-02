0d038fedd099bbfecb38c2558682357474f271bb
Adrien Duchemin <duchemin.adrien@gmail.com>
2020-05-02 02:25:42 +0200

    integrate lambda nest

--- deployment/src/lib/deployment-stack.ts
+++ deployment/src/lib/deployment-stack.ts
@@ -1,19 +1,28 @@
 
-import * as sns from '@aws-cdk/aws-sns'
-import * as subs from '@aws-cdk/aws-sns-subscriptions'
-import * as sqs from '@aws-cdk/aws-sqs'
-import { App, Duration, Stack, StackProps } from '@aws-cdk/core'
+import { Code, Function, Runtime } from '@aws-cdk/aws-lambda'
+import { App, Stack, StackProps } from '@aws-cdk/core'
+
+export interface DeploymentStackProps extends StackProps {
+  lambdaCodePath: string;
+  lambdaHandler?: string;
+}
 
 export class DeploymentStack extends Stack {
-  constructor(scope: App, id: string, props?: StackProps) {
+  constructor(scope: App, id: string, props: DeploymentStackProps) {
     super(scope, id, props)
 
-    const queue = new sqs.Queue(this, 'queuee', {
-      visibilityTimeout: Duration.seconds(300)
+    const { lambdaCodePath, lambdaHandler = 'lambda.handler' } = props
+    const lambda = new Function(this, 'lambda', {
+      runtime: Runtime.NODEJS_12_X,
+      code: Code.fromAsset(lambdaCodePath, { exclude: [
+        'cdk.out'
+      ] }),
+      handler: lambdaHandler
     })
 
-    const topic = new sns.Topic(this, 'topic')
-
-    topic.addSubscription(new subs.SqsSubscription(queue))
+    console.log('Lambda ARN', { arn: lambda.functionArn })
+    // link lambda to a generic sqs
+    // push the log into the generic sqs
+    // ce generic sqs implement le sqs with log du tuto youtube
   }
 }
--- deployment/src/main.ts
+++ deployment/src/main.ts
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import { join } from 'path'
 import { App } from '@aws-cdk/core'
 
 import { name } from '../package.json'
@@ -7,6 +8,4 @@ import { DeploymentStack } from './lib/deployment-stack'
 const app = new App()
 // care when changing the name in package json, it wont destroy the previous stack
 // so do it before changing : npm run cdk destroy ${old-name}
-new DeploymentStack(app, `${name}`)
-
-// add the path to the lambda code as prop
+new DeploymentStack(app, `${name}`, { lambdaCodePath: join(__dirname, '../../dist') })
