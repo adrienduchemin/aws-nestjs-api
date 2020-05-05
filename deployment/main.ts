922bfe89700fa121ebb86b91499cc4c4bf6ba5f7
Adrien Duchemin <duchemin.adrien@gmail.com>
2020-05-05 11:06:55 +0200

    move to my stack

--- deployment/main.ts
+++ deployment/main.ts
@@ -1,17 +1,10 @@
-import { join } from "path"
-import { Code } from "@aws-cdk/aws-lambda"
 import { App } from "@aws-cdk/core"
-//to refacto with lerna lib
-import { ApiLambdaWithHistoryQueueStack } from "../../aws-nestjs-generic-deployment/dist/stacks/api-lambda-with-history-queue.stack"
+
 import { name } from "../package.json"
+import { MyStack } from "./stacks/stack"
 
 const app = new App()
 
-new ApiLambdaWithHistoryQueueStack(app, name, {
-    lambdaProps : {
-        code: Code.fromAsset(join(__dirname, '../../bundle.zip')), //from env ci
-        handler: 'dist/src/lambda.handler',
-    },
-})
+new MyStack(app, name)
 
 export default app
